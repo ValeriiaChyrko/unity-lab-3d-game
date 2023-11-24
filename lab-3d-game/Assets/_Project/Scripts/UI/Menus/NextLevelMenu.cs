c541c2d89cd44721b1fc1ca834e55ee37c993856
Valeriia Chyrko <valeriia.chyrko@oa.edu.ua>
2023-11-24 13:30:20 +0200

    Add Universal Render Pipeline (URP) and include effects such as Post-Processing, Bloom, Channel Mixer and Motion Blur

--- lab-3d-game/Assets/_Project/Scripts/UI/Menus/NextLevelMenu.cs
+++ lab-3d-game/Assets/_Project/Scripts/UI/Menus/NextLevelMenu.cs
@@ -1,4 +1,6 @@
-﻿using Platformer._Project.Scripts.Utils;
+﻿using System.IO;
+using Platformer._Project.Scripts.Utils;
+using Platformer._Project.Scripts.Utils.Entity;
 using Platformer._Project.Settings;
 using TMPro;
 using UnityEngine;
@@ -8,9 +10,54 @@ namespace Platformer._Project.Scripts.UI.Menus
     public class NextLevelMenu : MonoBehaviour
     {
         [SerializeField] private TextMeshProUGUI scoreText;
+        [SerializeField] private TextMeshProUGUI maxScoreText;
+        
+        private const string PATH = "/player/data.json";
+
+        private int _currentScore;
+        private int _maxScore;
+
+        private void Awake()
+        {
+            Time.timeScale = 0;
+            _currentScore = GameManager.Instance.Score;
+            _maxScore = GetMaxScore();
+            
+            scoreText.text = "Набрано балів " + _currentScore;
+            maxScoreText.text = "Найкращий результат " + _maxScore;
+
+            _maxScore = _currentScore > _maxScore ? _currentScore : _maxScore;
+            WriteToFile();
+        }
+
+        private int GetMaxScore()
+        {
+            var filePath = Application.persistentDataPath + PATH;
+            
+            if (!File.Exists(filePath)) return 0;
+            
+            var json = File.ReadAllText(filePath);
+            var data = JsonUtility.FromJson<PlayerData>(json);
+            return data.maxScore;
+
+        }
+
+        private void WriteToFile()
+        {
+            var data = new PlayerData
+            {
+                name = "Any", 
+                currentScore = _currentScore,
+                maxScore = _maxScore,
+                healthRate = Health.CurrentHealth
+            };
+            var json = JsonUtility.ToJson(data);
+            var filePath = Application.persistentDataPath + PATH;
+            File.WriteAllText(filePath, json);
+        }
+
         public void OnClickNextLevel()
         {
-            scoreText.text = GameManager.Instance.Score.ToString();
             ScenesManager.Instance.LoadNextLevel();
         }
     }
